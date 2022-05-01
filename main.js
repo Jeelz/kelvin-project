dc92bce5df9a4f2b7c0af714090115ed894f46ef
Jeelz <56708384+Jeelz@users.noreply.github.com>
2022-05-01 12:09:15 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+const kelvin = 293;
+const celsius = kelvin - 273;
+var farenheit = celsius * (9/5) + 32;
+farenheit = Math.floor(farenheit);
+console.log(`The tempreature is ${farenheit} degrees Farenheit`)
